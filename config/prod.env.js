b4b8852772728b08011179a90a7f84f8a7bb935c
zhouxiaofu1 <wuhan2583@126.com>
2019-11-11 18:02:41 +0800

    11

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://api.auauz.net"'
+  // BASE_API: '"https://api.auauz.net"'
+  BASE_API: '"http://localhost:8000"'
 }
